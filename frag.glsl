63e6c52b629e2493a9884cd01c3415d3de6101c2
Tom Hutman <tom@ath92.nl>
2020-08-28 22:02:19 +0200

    water

--- frag.glsl
+++ frag.glsl
@@ -1,7 +1,14 @@
 precision highp float;
-uniform vec2 mousePosition;
+uniform float time;
+varying vec2 uv;
+uniform sampler2D texture;
 
 void main() {
-    float d = distance(mousePosition, gl_FragCoord.xy) / 1000.;
-    gl_FragColor = vec4(d, 1., 0., 1.);
+    float d = distance(vec2(0, 0), uv);
+    // float g = sin(gl_FragCoord.y * d * (sin(time / 10000. - 1.6))) / 10.;
+    float hor = sin(gl_FragCoord.x / 100.) * d / 2.;
+    float ver = cos(gl_FragCoord.y / 100.) * d / 2.;
+    float g = hor * ver;
+    vec4 color = texture2D(texture, vec2(.5 + .5 * uv.x, .5 + .5 * -uv.y) + vec2(g));
+    gl_FragColor = color;
 }
--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 import Regl from "regl";
+import resl from "resl";
 import passThroughVert from "./pass-through-vert.glsl";
 import frag from "./frag.glsl";
 
-const regl = Regl();
+const regl = Regl({
+    pixelRatio: 1,
+});
 
 const screenFillingRectangle = regl.buffer([
     [-1, -1], [1, -1], [1,  1],
     [-1, -1], [1, 1,], [-1, 1]
 ]);
-    
+
 const render = regl({
     frag,
     vert: passThroughVert,
     uniforms: {
-        mousePosition: regl.prop('mousePosition'),
+        // mousePosition: regl.prop('mousePosition'),
+        time: regl.prop('time'),
+        texture: regl.prop('video'),
     },
     attributes: {
         position: screenFillingRectangle,
@@ -28,14 +33,28 @@ const mouse = {
 
 window.onload = () => {
     document.addEventListener("mousemove", (e) => {
-        mouse.x = e.screenX;
-        mouse.y = e.screenY;
+        mouse.x = e.clientX;
+        mouse.y = e.clientY;
     });
 };
 
-requestAnimationFrame(function loop() {
-    render({
-        mousePosition: [mouse.x, -mouse.y],
-    });
-    requestAnimationFrame(loop);
-});
+(async () => {
+    const video = document.createElement('video');
+    try {
+        video.srcObject = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+        document.body.appendChild(video);
+        video.onloadedmetadata = () => {
+            video.play();
+            const webcam = regl.texture(video);
+            regl.frame(() => {
+                webcam.subimage(video);
+                render({ 
+                    time: performance.now(),
+                    video: webcam
+                });
+            });
+        };
+    } catch (e) {
+        console.log(e)
+    }
+})();
